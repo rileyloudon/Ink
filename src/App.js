2088052150b0c0fc61359916c91b72313eabd252
Riley Loudon <rileyloudon@icloud.com>
2021-09-01 21:38:37 -0500

    setup react router dom for Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
