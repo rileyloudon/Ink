f937f60684a281d5c3f0bfdad759a18aad1ba796
Riley Loudon <rileyloudon@icloud.com>
2021-09-03 21:44:56 -0500

    update routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Register from './components/Register';
+import Inbox from './components/Inbox';
 import './App.css';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <div className='App'>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/inbox' component={Inbox} />
         </Switch>
       </div>
     </BrowserRouter>
