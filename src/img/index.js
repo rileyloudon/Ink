ff89245ad30c7c984a04e050699c317cf2d5cf97
Riley Loudon <rileyloudon@icloud.com>
2021-09-12 20:36:04 -0500

    setup import/export of images

---
+++ src/img/index.js
@@ -0,0 +1,31 @@
+import homeNotActive from './home/dark-home-na.svg';
+import homeActive from './home/dark-home-a.svg';
+
+import chatNotActive from './chat/dark-chat-na.svg';
+import chatActive from './chat/dark-chat-a.svg';
+
+import addNotActive from './add/dark-add-na.svg';
+import addActive from './add/dark-add-a.svg';
+
+import favoriteNotActive from './favorite/dark-favorite-na.svg';
+import favoriteActive from './favorite/dark-favorite-a.svg';
+
+export const home = {
+  homeNotActive,
+  homeActive,
+};
+
+export const chat = {
+  chatNotActive,
+  chatActive,
+};
+
+export const add = {
+  addNotActive,
+  addActive,
+};
+
+export const favorite = {
+  favoriteNotActive,
+  favoriteActive,
+};
