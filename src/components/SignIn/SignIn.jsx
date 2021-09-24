5727be0483f3ebb18feefb6d2a71518f932645dc
Riley Loudon <rileyloudon@icloud.com>
2021-09-23 22:02:38 -0500

    move signin props inside component

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { signInUser } from '../../firebase';
 import astronaut from '../../img/misc/astronaut.svg';
 import './SignIn.css';
 
-const SignIn = ({ handleSignIn, signInError }) => {
+const SignIn = ({ updateLoading, signInGuest }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [signInError, setSignInError] = useState('');
 
   const isFormValid =
     /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g.test(email) &&
-    password.length > 6;
+    password.length >= 6;
+
+  const handleSignIn = () => {
+    updateLoading(true);
+    signInUser(email, password).then((res) => {
+      if (typeof res === 'string') setSignInError(res);
+      updateLoading(false);
+    });
+  };
 
   return (
     <div className='sign-in-container'>
@@ -43,7 +53,9 @@ const SignIn = ({ handleSignIn, signInError }) => {
               className={isFormValid ? 'log-in-btn active' : 'log-in-btn'}
               type='submit'
               disabled={!isFormValid}
-              onClick={() => handleSignIn(email, password)}
+              onClick={() => {
+                if (isFormValid) handleSignIn(email, password);
+              }}
             >
               Log In
             </button>
@@ -55,7 +67,7 @@ const SignIn = ({ handleSignIn, signInError }) => {
             <button
               className='guest-btn'
               type='button'
-              onClick={() => handleSignIn('Guest')}
+              onClick={() => signInGuest()}
             >
               <img src={astronaut} alt='' />
               Log In as Guest
@@ -80,13 +92,9 @@ const SignIn = ({ handleSignIn, signInError }) => {
   );
 };
 
-SignIn.defaultProps = {
-  signInError: '',
-};
-
 SignIn.propTypes = {
-  handleSignIn: PropTypes.func.isRequired,
-  signInError: PropTypes.string,
+  updateLoading: PropTypes.func.isRequired,
+  signInGuest: PropTypes.func.isRequired,
 };
 
 export default SignIn;
