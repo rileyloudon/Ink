b5e9d3a171948205261ef47e73a890b455e175d9
Riley Loudon <rileyloudon@icloud.com>
2021-09-25 21:33:44 -0500

    setup context, set user, update paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,64 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import Home from './components/Home';
-import Register from './components/Register';
-import Inbox from './components/Inbox';
+import Register from './components/Register/Register';
+import Chat from './components/Chat';
+import Header from './components/Header/Header';
+import UserContext from './Context/UserContext';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const auth = getAuth();
+
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) setUser(currentUser);
+    else setUser();
+  });
+
+  const updateLoading = (value) => {
+    setLoading(value);
+  };
+
+  const signInGuest = () => {
+    // 'Sign in as guest' -> account named @Guest
+    // Sign In Guest Here
+  };
+
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className='App'>
+    <div className='App'>
+      <UserContext.Provider value={{ user, setUser }}>
+        {user && <Header />}
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/inbox' component={Inbox} />
+          <Route
+            exact
+            path='/'
+            render={() => (
+              <Home
+                updateLoading={updateLoading}
+                loading={loading}
+                signInGuest={signInGuest}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/register'
+            render={() => (
+              <Register
+                updateLoading={updateLoading}
+                signInGuest={signInGuest}
+              />
+            )}
+          />
+          <Route exact path='/chat' render={() => <Chat />} />
+          {/* <Route exact path='/:username' /> */}
         </Switch>
-      </div>
-    </BrowserRouter>
+      </UserContext.Provider>
+    </div>
   );
 }
 
