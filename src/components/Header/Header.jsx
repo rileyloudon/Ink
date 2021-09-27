28c18a470d39db2ff5835f3eeacc1dc0423ba416
Riley Loudon <rileyloudon@icloud.com>
2021-09-26 21:56:50 -0500

    refactor to remove props

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,61 +1,64 @@
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+// import { signOutUser } from '../../firebase';
 import { home, chat, add, favorite } from '../../img/index';
+import UserContext from '../../Context/UserContext';
 import './header.css';
 
-const Header = ({ profilePicture, active }) => (
-  <header>
-    <div className='header-container'>
-      <p className='ink title'>Ink</p>
+const Header = () => {
+  const { user } = useContext(UserContext);
+  const [activeTab, setActiveTab] = useState('home');
 
-      <input
-        className='search-box'
-        type='text'
-        placeholder='Search'
-        autoCapitalize='none'
-      />
-      <div className='nav'>
-        <Link to='/'>
-          <img
-            src={active === 'home' ? home.homeActive : home.homeNotActive}
-            alt='Home'
-          />
-        </Link>
-        <Link to='/chat'>
-          <img
-            src={active === 'chat' ? chat.chatActive : chat.chatNotActive}
-            alt='Chat'
-          />
-        </Link>
+  return (
+    <header>
+      <div className='header-container'>
+        <p className='ink title'>Ink</p>
 
-        {/* onClick -> Pop up modal */}
-        <img
-          src={active === 'add' ? add.addActive : add.addNotActive}
-          alt='Add'
+        <input
+          className='search-box'
+          type='text'
+          placeholder='Search'
+          autoCapitalize='none'
         />
 
-        {/* onClick -> Display follow requests, likes */}
-        <img
-          src={
-            active === 'favorite'
-              ? favorite.favoriteActive
-              : favorite.favoriteNotActive
-          }
-          alt='Heart'
-        />
+        <div className='nav'>
+          <Link to='/' onClick={() => setActiveTab('home')}>
+            <img
+              src={activeTab === 'home' ? home.homeActive : home.homeNotActive}
+              alt='Home'
+            />
+          </Link>
+          <Link to='/chat' onClick={() => setActiveTab('chat')}>
+            <img
+              src={activeTab === 'chat' ? chat.chatActive : chat.chatNotActive}
+              alt='Chat'
+            />
+          </Link>
 
-        {/* onClick => Display Profile, Saved Pictures (?), Settings, Log Out */}
-        <div className='profile-border'>
-          <img className='profile-picture' src={profilePicture} alt='' />
+          {/* onClick -> Pop up modal */}
+          <img
+            src={activeTab === 'add' ? add.addActive : add.addNotActive}
+            alt='Add'
+          />
+
+          {/* onClick -> Display follow requests, likes */}
+          <img
+            src={
+              activeTab === 'favorite'
+                ? favorite.favoriteActive
+                : favorite.favoriteNotActive
+            }
+            alt='Heart'
+          />
+
+          {/* onClick => Display Profile, Saved Pictures (?), Settings, Log Out */}
+          <div className='profile-border'>
+            <img className='profile-picture' src={user.photoURL} alt='' />
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-);
-
-Header.propTypes = {
-  active: PropTypes.string.isRequired,
-  profilePicture: PropTypes.string.isRequired,
+    </header>
+  );
 };
 
 export default Header;
