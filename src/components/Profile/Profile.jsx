63cfbdb13fc6ea53d166ea6efce1e046acdf69f4
Riley Loudon <rileyloudon@icloud.com>
2021-10-02 19:53:41 -0500

    fix profile not rendering

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,43 +1,50 @@
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { fetchUserData } from '../../firebase';
+import './Profile.css';
 
 const Profile = () => {
   const location = useLocation();
   const [usersProfile, setUsersProfile] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchUserData(location.pathname.substring(1)).then((res) =>
-      setUsersProfile(res)
-    );
+    fetchUserData(location.pathname.substring(1)).then((res) => {
+      setUsersProfile(res);
+      setLoading(false);
+    });
   }, [location.pathname]);
 
-  return (
-    <div className='profile'>
-      <header className='profile-header'>
-        <div className='left'>
-          <img className='picture' src={usersProfile.photoURL} alt='' />
-        </div>
-        <section className='right'>
-          <div className='top'>
-            <h3 className='username'>{usersProfile.username}</h3>
-            <button className='follow' type='button'>
-              Follow
-            </button>
+  const renderProflie = () => {
+    return (
+      <div className='profile'>
+        <header className='profile-header'>
+          <div className='left'>
+            <img className='picture' src={usersProfile.photoURL} alt='' />
           </div>
-          <ul className='stats'>
-            <li>{usersProfile.posts.length} posts</li>
-            <li>{usersProfile.followers.length} followers</li>
-            <li>{usersProfile.following.length} following</li>
-          </ul>
-        </section>
-        <div className='info'>
-          <h4>{usersProfile.fullName}</h4>
-          <p>{usersProfile.bio}</p>
-        </div>
-      </header>
-    </div>
-  );
+          <section className='right'>
+            <div className='top'>
+              <h3 className='username'>{usersProfile.username}</h3>
+              <button className='follow' type='button'>
+                Follow
+              </button>
+            </div>
+            <ul className='stats'>
+              <li>{usersProfile.posts.length} posts</li>
+              <li>{usersProfile.followers.length} followers</li>
+              <li>{usersProfile.following.length} following</li>
+            </ul>
+          </section>
+          <div className='info'>
+            <h4>{usersProfile.fullName}</h4>
+            {/* <p>{usersProfile.bio}</p> */}
+          </div>
+        </header>
+      </div>
+    );
+  };
+
+  return !loading && renderProflie();
 };
 
 export default Profile;
