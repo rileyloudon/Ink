5d99207b1b24201adf7979eb8e6ae0ea7ece0da1
Riley Loudon <rileyloudon@icloud.com>
2021-10-05 22:22:30 -0500

    use route for active tab

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import DropDown from '../DropDown/DropDown';
 import { home, chat, add, favorite } from '../../img/index';
 import UserContext from '../../Context/UserContext';
 import './header.css';
 
 const Header = () => {
+  const location = useLocation();
   const { user } = useContext(UserContext);
 
-  const [activeTab, setActiveTab] = useState('home');
+  const [activeTab] = useState();
 
   const [displayUserDropdown, setDisplayUserDropdown] = useState(false);
   const closeDropDown = () => setDisplayUserDropdown(false);
@@ -17,7 +18,9 @@ const Header = () => {
     <header>
       <div className='center-header'>
         <div className='header-container'>
-          <p className='ink title'>Ink</p>
+          <Link to='/' className='title'>
+            <p className='ink'>Ink</p>
+          </Link>
 
           <input
             className='search-box'
@@ -27,20 +30,24 @@ const Header = () => {
           />
 
           <div className='nav'>
-            <Link to='/' onClick={() => setActiveTab('home')}>
+            <Link to='/'>
               <img
                 src={
-                  activeTab === 'home' && !displayUserDropdown
+                  location.pathname === '/' &&
+                  !displayUserDropdown &&
+                  (activeTab !== 'add' || activeTab !== 'favorite')
                     ? home.homeActive
                     : home.homeNotActive
                 }
                 alt='Home'
               />
             </Link>
-            <Link to='/chat' onClick={() => setActiveTab('chat')}>
+            <Link to='/chat'>
               <img
                 src={
-                  activeTab === 'chat' && !displayUserDropdown
+                  location.pathname === '/chat' &&
+                  !displayUserDropdown &&
+                  (activeTab !== 'add' || activeTab !== 'favorite')
                     ? chat.chatActive
                     : chat.chatNotActive
                 }
@@ -48,13 +55,13 @@ const Header = () => {
               />
             </Link>
 
-            {/* onClick -> Pop up modal */}
+            {/* onClick -> Pop up modal, setActiveTab('add') */}
             <img
               src={activeTab === 'add' ? add.addActive : add.addNotActive}
               alt='Add'
             />
 
-            {/* onClick -> Display follow requests, likes */}
+            {/* onClick -> Display follow requests, likes, setActiveTab('favorite') */}
             <img
               src={
                 activeTab === 'favorite'
@@ -67,18 +74,17 @@ const Header = () => {
             <button
               className='profile-border'
               type='button'
-              onClick={() =>
-                displayUserDropdown
-                  ? setDisplayUserDropdown(false)
-                  : setDisplayUserDropdown(true)
-              }
+              onClick={() => {
+                if (displayUserDropdown) setDisplayUserDropdown(false);
+                else setDisplayUserDropdown(true);
+              }}
             >
               <img className='profile-picture' src={user.photoURL} alt='' />
             </button>
+            {displayUserDropdown && <DropDown closeDropDown={closeDropDown} />}
           </div>
         </div>
       </div>
-      {displayUserDropdown && <DropDown closeDropDown={closeDropDown} />}
     </header>
   );
 };
