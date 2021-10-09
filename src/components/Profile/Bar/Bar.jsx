8811258447654dc0daad865833479ddc90e8a589
Riley Loudon <rileyloudon@icloud.com>
2021-10-08 22:23:22 -0500

    setup bar for posts

---
+++ src/components/Profile/Bar/Bar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Bar.css';
+
+const Bar = () => {
+  return (
+    <div className='profile-tabs' role='tablist'>
+      <p>Posts</p>
+    </div>
+  );
+};
+
+export default Bar;
