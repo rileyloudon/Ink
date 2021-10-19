a654dff453361dc88f6ac8a4abc2711021c20d7c
Riley Loudon <rileyloudon@icloud.com>
2021-10-18 22:03:52 -0500

    use new component for dropzone

--- src/components/AddPhoto/AddPhoto.jsx
+++ src/components/AddPhoto/AddPhoto.jsx
@@ -1,31 +1,13 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import { useRef, useEffect, useState, useCallback } from 'react';
-import { useDropzone } from 'react-dropzone';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { photo } from '../../img/index';
+import DropImage from './DropImage/DropImage';
 import './AddPhoto.css';
 
 const AddPhoto = ({ updateAddModal }) => {
   const modal = useRef();
-  const [dropRejected, setDropRejected] = useState(false);
   const [image, setImage] = useState();
 
-  const onDropAccepted = useCallback((acceptedFiles) => {
-    setImage(URL.createObjectURL(acceptedFiles[0]));
-  }, []);
-
-  const onDropRejected = useCallback(() => {
-    setDropRejected(true);
-  }, []);
-
-  const { getRootProps, getInputProps, open } = useDropzone({
-    multiple: false,
-    accept: 'image/jpeg, image/png',
-    noClick: true,
-    noKeyboard: true,
-    onDropAccepted,
-    onDropRejected,
-  });
+  const updateImage = (value) => setImage(value);
 
   useEffect(() => {
     const handleClickOutside = (e) => {
@@ -43,45 +25,15 @@ const AddPhoto = ({ updateAddModal }) => {
 
   return (
     <div ref={modal} className='dropzone-container'>
-      <div {...getRootProps({ className: 'dropzone' })}>
-        <button
-          className='close'
-          aria-label='close'
-          type='button'
-          onClick={() => updateAddModal(false)}
-        >
-          &#x2715;
-        </button>
-        <input {...getInputProps()} />
-        {!image && (
-          <>
-            <img
-              src={!dropRejected ? photo.photoDefault : photo.photoError}
-              alt=''
-            />
-            <h3>
-              {!dropRejected
-                ? 'Drag Photos Here.'
-                : 'This file type is not supported.'}
-            </h3>
-            {dropRejected && <p>Please use .jpeg or .png</p>}
-            <button
-              className='select-from-computer'
-              type='button'
-              onClick={open}
-            >
-              Select From Computer
-            </button>
-          </>
-        )}
-        {image && (
-          <img
-            style={{ width: '150px', height: 'auto' }}
-            src={image}
-            alt='test'
-          />
-        )}
-      </div>
+      {!image ? (
+        <DropImage updateImage={updateImage} updateAddModal={updateAddModal} />
+      ) : (
+        <img
+          style={{ width: '150px', height: 'auto' }}
+          src={image}
+          alt='test'
+        />
+      )}
     </div>
   );
 };
