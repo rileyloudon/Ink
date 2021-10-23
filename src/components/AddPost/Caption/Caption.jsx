22c46e12920b7a8eb2214bd136b24ab1f96e15f2
Riley Loudon <rileyloudon@icloud.com>
2021-10-22 21:40:41 -0500

    allow submitting of posts

--- src/components/AddPost/Caption/Caption.jsx
+++ src/components/AddPost/Caption/Caption.jsx
@@ -1,18 +1,29 @@
 import PropTypes from 'prop-types';
 import { useContext, useState } from 'react';
 import UserContext from '../../../Context/UserContext';
+import { savePost } from '../../../firebase';
 import './Caption.css';
 
-const Caption = ({ image }) => {
+const Caption = ({ image, updateAddModal }) => {
   const { user } = useContext(UserContext);
 
   const [text, setText] = useState('');
   const [disableCommenting, setDisableCommenting] = useState(false);
 
+  const [error, setError] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    savePost(image, text, disableCommenting).then((res) => {
+      if (typeof res === 'string') setError(res);
+      else updateAddModal(false);
+    });
+  };
+
   return (
     <div className='caption'>
       <figure className='image'>
-        <img src={image} alt='test' />
+        <img src={image.url} alt='test' />
       </figure>
       <form className='form'>
         <img src={user.photoURL} alt='' />
@@ -35,22 +46,23 @@ const Caption = ({ image }) => {
             onChange={(e) => setDisableCommenting(e.target.checked)}
           />
         </label>
-        <button
-          type='submit'
-          onClick={(e) => {
-            e.preventDefault();
-            console.log(text, disableCommenting);
-          }}
-        >
+        <button type='submit' onClick={(e) => handleSubmit(e)}>
           Share
         </button>
+        {error && <p>{error}</p>}
       </form>
     </div>
   );
 };
 
 Caption.propTypes = {
-  image: PropTypes.string.isRequired,
+  image: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    properties: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  updateAddModal: PropTypes.func.isRequired,
 };
 
 export default Caption;
