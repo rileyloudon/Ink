42957d4adeacef6ff0b0c8e0a96f8ed0d5d0035e
Riley Loudon <rileyloudon@icloud.com>
2021-10-24 21:55:01 -0500

    update to use async await instead of .then

--- src/firebase.js
+++ src/firebase.js
@@ -35,9 +35,6 @@ const firebaseConfig = {
 
 initializeApp(firebaseConfig);
 const db = getFirestore();
-const storage = getStorage();
-const storageRef = ref(storage);
-console.log(storageRef);
 
 export const registerUser = async (email, password, tempUsername, fullName) => {
   const username = tempUsername.toLowerCase();
@@ -47,58 +44,69 @@ export const registerUser = async (email, password, tempUsername, fullName) => {
   if (docSnap.exists()) return 'A user with that username already exists.';
 
   const auth = getAuth();
-  return createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      return updateProfile(auth.currentUser, {
+
+  try {
+    const credential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    try {
+      await updateProfile(auth.currentUser, {
         displayName: username,
         photoURL: `https://source.boringavatars.com/beam/150/${username}?colors=2D1B33,F36A71,EE887A,E4E391,9ABC8A`,
-      }).then(() => {
-        try {
-          setDoc(doc(db, 'users', username), {
-            username,
-            photoURL: `https://source.boringavatars.com/beam/150/${username}?colors=2D1B33,F36A71,EE887A,E4E391,9ABC8A`,
-            fullName,
-            posts: [],
-            followers: [],
-            following: [],
-            bio: '',
-          });
-        } catch (err) {
-          console.error(err);
-        }
-        return userCredential;
       });
-    })
-    .catch((err) => {
-      switch (err.code) {
-        case 'auth/email-already-in-use':
-          return `A user with that email already exists.`;
-        case 'auth/invalid-email':
-          return 'Enter a valid email address.';
-        case 'auth/weak-password':
-          return 'Please enter a stonger password.';
-        default:
-          return 'Error';
-      }
-    });
+    } catch (err) {
+      return `Error updating profile, ${err}`;
+    }
+
+    try {
+      setDoc(doc(db, 'users', username), {
+        username,
+        photoURL: `https://source.boringavatars.com/beam/150/${username}?colors=2D1B33,F36A71,EE887A,E4E391,9ABC8A`,
+        fullName,
+        posts: [],
+        followers: [],
+        following: [],
+        bio: '',
+      });
+    } catch (err) {
+      return `Error updating profile, ${err}`;
+    }
+    return credential;
+  } catch (err) {
+    switch (err.code) {
+      case 'auth/email-already-in-use':
+        return `A user with that email already exists.`;
+      case 'auth/invalid-email':
+        return 'Enter a valid email address.';
+      case 'auth/weak-password':
+        return 'Please enter a stonger password.';
+      default:
+        return 'Error';
+    }
+  }
 };
 
-export const signInUser = (email, password) => {
+export const signInUser = async (email, password) => {
   const auth = getAuth();
-  return signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => userCredential)
-    .catch((err) => {
-      switch (err.code) {
-        case 'auth/invalid-email':
-          return 'Enter a valid email address.';
-        case 'auth/user-not-found':
-          return "The email you entered doesn't belong to an account.";
-        case 'auth/wrong-password':
-          return 'Sorry, your password was incorrect. Please double-check your password.';
-        default:
-          return 'Error';
-      }
-    });
+
+  try {
+    const credential = await signInWithEmailAndPassword(auth, email, password);
+    return credential;
+  } catch (err) {
+    switch (err.code) {
+      case 'auth/invalid-email':
+        return 'Enter a valid email address.';
+      case 'auth/user-not-found':
+        return "The email you entered doesn't belong to an account.";
+      case 'auth/wrong-password':
+        return 'Sorry, your password was incorrect. Please double-check your password.';
+      default:
+        return 'Error';
+    }
+  }
 };
 
 export const signOutUser = () => {
