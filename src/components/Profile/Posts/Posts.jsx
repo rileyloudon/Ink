6fd80b06c4b14647e44ba26d6c8d4fac8f7e4f63
Riley Loudon <rileyloudon@icloud.com>
2021-10-29 23:12:49 -0500

    load more posts on scroll

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -3,10 +3,14 @@ import { useEffect, useState } from 'react';
 import './Posts.css';
 
 const Posts = ({ profile }) => {
-  const [first9Posts, setFirst9Posts] = useState([]);
-  // const [hiddenPosts, setHiddenPosts] = useState([]);
+  const [displayedPosts, setDisplayedPosts] = useState(
+    profile.posts.slice(0, 9)
+  );
+  const [hiddenPosts, setHiddenPosts] = useState(
+    profile.posts.slice(9, profile.posts.length + 1)
+  );
 
-  const initialPosts = first9Posts.map((post) => {
+  const renderPost = (post) => {
     return (
       <div className='post'>
         <img
@@ -17,14 +21,28 @@ const Posts = ({ profile }) => {
         />
       </div>
     );
-  });
+  };
 
   useEffect(() => {
-    setFirst9Posts(profile.posts.slice(0, 9).reverse());
-    // setHiddenPosts(profile.posts.slice(9, profile.posts.length + 1));
-  }, [profile.posts]);
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight
+      )
+        return;
+      const addedPosts = hiddenPosts.slice(0, 6);
+      setDisplayedPosts((oldArray) => [...oldArray, ...addedPosts]);
+      setHiddenPosts((oldArray) => oldArray.slice(6, profile.posts.length + 1));
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [profile.posts, hiddenPosts, displayedPosts]);
 
-  return <div className='posts'>{initialPosts}</div>;
+  return (
+    <div className='posts'>
+      {displayedPosts.map((post) => renderPost(post))}
+    </div>
+  );
 };
 
 Posts.propTypes = {
