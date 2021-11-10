4a43a73af733f6cec4ccd19f66c45d41e81c687c
Riley Loudon <rileyloudon@icloud.com>
2021-11-09 23:21:14 -0600

    add profile link, update state to use an object

--- src/components/ViewPost/SoloView/SoloView.jsx
+++ src/components/ViewPost/SoloView/SoloView.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import { fetchIndividualPost } from '../../../firebase';
 import Loading from '../../Loading/Loading';
 
@@ -7,32 +7,40 @@ import './SolowView.css';
 
 const SoloView = () => {
   const location = useLocation();
-  const [post, setPost] = useState();
+  const [postData, setPostData] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchIndividualPost(location.pathname).then((res) => {
-      setPost(res);
+      setPostData(res);
       setLoading(false);
     });
   }, [location.pathname]);
 
   const renderPost = () => {
-    return post === 'Post not found' || post === 'User not found' ? (
-      <div className='error'>
-        <h2>{post}.</h2>
+    return postData === 'Post not found' || postData === 'User not found' ? (
+      <div className='post-error'>
+        <h2>{postData}.</h2>
         <p>Please double check the URL to make sure it&#39;s correct.</p>
       </div>
     ) : (
       <div className='solo-view'>
         <figure className='post-image'>
-          <img src={post.imageUrl} alt='' />
+          <img src={postData.post.imageUrl} alt='' />
         </figure>
         <div className='comments'>
+          <div className='poster'>
+            <Link to={`/${postData.username}`}>
+              <img src={postData.photoURL} alt='' />
+            </Link>
+            <h3>
+              <Link to={`/${postData.username}`}>{postData.username}</Link>
+            </h3>
+          </div>
           {/* <p>MAP COMMENTS</p> */}
           <section className='add-comment'>
             <textarea placeholder='Add a comment...' />
-            <button className='post' type='button' disabled>
+            <button className='post-btn' type='button' disabled>
               Post
             </button>
           </section>
