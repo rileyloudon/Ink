306a9c36d08e84afb60c0e8e6b49c7f4a970c43d
Riley Loudon <rileyloudon@icloud.com>
2021-11-11 22:28:20 -0600

    create comment component, setup emoji-mart

---
+++ src/components/AddComment/AddComment.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Picker } from 'emoji-mart';
+import { emoji } from '../../img';
+import './AddComment.css';
+import 'emoji-mart/css/emoji-mart.css';
+
+const AddComment = () => {
+  const [comment, setComment] = useState('');
+  const [displayEmojiPicker, setDisplayEmojiPicker] = useState(false);
+
+  const selectEmoji = (emojiObject) => {
+    setComment((currentComment) => currentComment + emojiObject.native);
+  };
+
+  return (
+    <>
+      {displayEmojiPicker && (
+        <Picker
+          showPreview={false}
+          showSkinTones={false}
+          native='true'
+          sheetSize='16'
+          perLine='6'
+          onSelect={selectEmoji}
+        />
+      )}
+
+      <section className='add-comment'>
+        <button
+          type='button'
+          className='emoji-btn'
+          onClick={() =>
+            displayEmojiPicker
+              ? setDisplayEmojiPicker(false)
+              : setDisplayEmojiPicker(true)
+          }
+        >
+          <img src={emoji.darkFace} alt='' />
+        </button>
+        <textarea
+          placeholder='Add a comment...'
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button
+          className='post-btn'
+          type='button'
+          disabled={comment.length < 1}
+        >
+          Post
+        </button>
+      </section>
+    </>
+  );
+};
+
+export default AddComment;
