23c9b0116d36c0229fa79fc06b953d7887e41079
Riley Loudon <rileyloudon@icloud.com>
2021-11-17 23:11:08 -0600

    update SoloView to work as a modal or single page

--- src/components/ViewPost/SoloView/SoloView.jsx
+++ src/components/ViewPost/SoloView/SoloView.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation, Link } from 'react-router-dom';
-import { fetchIndividualPost } from '../../../firebase';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { useLocation, useHistory, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { fetchIndividualPost, toggleLikePost } from '../../../firebase';
 import { favorite, comment } from '../../../img';
+import UserContext from '../../../Context/UserContext';
 import AddComment from '../../AddComment/AddComment';
 import Loading from '../../Loading/Loading';
 
 import './SolowView.css';
 
-const SoloView = () => {
+const SoloView = ({ type }) => {
+  const { user } = useContext(UserContext);
+  const modal = useRef();
   const location = useLocation();
+  const history = useHistory();
+
   const [postData, setPostData] = useState();
   const [loading, setLoading] = useState(true);
 
@@ -59,6 +65,16 @@ const SoloView = () => {
     });
   }, [location.pathname]);
 
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (modal.current && !modal.current.contains(e.target)) history.goBack();
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [history]);
+
   const renderPost = () => {
     return postData === 'Post not found' || postData === 'User not found' ? (
       <div className='post-error'>
@@ -66,53 +82,77 @@ const SoloView = () => {
         <p>Please double check the URL to make sure it&#39;s correct.</p>
       </div>
     ) : (
-      <div className='solo-view'>
-        <figure className='post-image' onDoubleClick={() => console.log('hi')}>
-          <img src={postData.post.imageUrl} alt='' />
-        </figure>
-        <section className='side-bar'>
-          <section className='poster'>
-            <Link to={`/${postData.username}`}>
-              <img src={postData.photoURL} alt='' />
-            </Link>
-            <Link to={`/${postData.username}`}>
-              <span>{postData.username}</span>
-            </Link>
+      <>
+        {type === 'modal' && <div className='post-backdrop' />}
+        <div
+          ref={type === 'modal' ? modal : null}
+          className={type === 'modal' ? 'solo-view modal-view' : 'solo-view'}
+        >
+          <figure
+            className='post-image'
+            onDoubleClick={() =>
+              toggleLikePost(postData.username, location.pathname)
+            }
+          >
+            <img src={postData.post.imageUrl} alt='' />
+          </figure>
+          <section className='side-bar'>
+            <section className='poster'>
+              <Link to={`/${postData.username}`}>
+                <img src={postData.photoURL} alt='' />
+              </Link>
+              <Link to={`/${postData.username}`}>
+                <span>{postData.username}</span>
+              </Link>
+            </section>
+            <section className='comments' />
+            <section className='interact'>
+              <div className='icons'>
+                <button
+                  type='button'
+                  onClick={() =>
+                    toggleLikePost(postData.username, location.pathname)
+                  }
+                >
+                  <img
+                    src={
+                      postData.post.likes.includes(user.displayName)
+                        ? favorite.darkLikedFavorite
+                        : favorite.favoriteNotActive
+                    }
+                    alt=''
+                  />
+                </button>
+                <button
+                  type='button'
+                  onClick={() => document.getElementById('textarea').focus()}
+                >
+                  <img src={comment.darkTextOutline} alt='' />
+                </button>
+              </div>
+              <div className='likes'>
+                <p>{likeCount()}</p>
+              </div>
+              <div className='date'>
+                <time dateTime={postData.post.timestamp.toDate()}>
+                  {postedDate()}
+                </time>
+              </div>
+            </section>
+            <section className='comment-box'>
+              <AddComment />
+            </section>
           </section>
-          <section className='comments' />
-          <section className='interact'>
-            <div className='icons'>
-              <button
-                type='button'
-                // onClick={LIKE POST}
-              >
-                <img src={favorite.favoriteNotActive} alt='' />
-              </button>
-              <button
-                type='button'
-                onClick={() => document.getElementById('textarea').focus()}
-              >
-                <img src={comment.darkTextOutline} alt='' />
-              </button>
-            </div>
-            <div className='likes'>
-              <p>{likeCount()}</p>
-            </div>
-            <div className='date'>
-              <time dateTime={postData.post.timestamp.toDate()}>
-                {postedDate()}
-              </time>
-            </div>
-          </section>
-          <section className='comment-box'>
-            <AddComment />
-          </section>
-        </section>
-      </div>
+        </div>
+      </>
     );
   };
 
   return loading ? <Loading /> : renderPost();
+};
+
+SoloView.propTypes = {
+  type: PropTypes.string.isRequired,
 };
 
 export default SoloView;
