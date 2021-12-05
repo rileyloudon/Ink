9634245b2d0e8135cfbcea555caab9bec5fd60a9
Riley Loudon <rileyloudon@icloud.com>
2021-12-04 22:15:36 -0600

    create Owner, returns post owner

---
+++ src/components/Post/Owner/Owner.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import './Owner.css';
+
+const Owner = ({ owner, profilePicture }) => {
+  return (
+    <section className='owner'>
+      <Link to={`/${owner}`}>
+        <img src={profilePicture} alt='' />
+      </Link>
+      <Link to={`/${owner}`}>
+        <span>{owner}</span>
+      </Link>
+    </section>
+  );
+};
+
+Owner.propTypes = {
+  owner: PropTypes.string.isRequired,
+  profilePicture: PropTypes.string.isRequired,
+};
+
+export default Owner;
