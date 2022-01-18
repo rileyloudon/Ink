2cbd6d691985ba51f6703876e5623442d4758bc5
Riley Loudon <rileyloudon@icloud.com>
2022-01-17 21:40:40 -0600

    rename to UserDropDown

--- src/components/Header/UserDropDown/UserDropDown.jsx
+++ src/components/Header/UserDropDown/UserDropDown.jsx
@@ -2,10 +2,10 @@ import { useContext, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { signOutUser } from '../../../firebase';
-import './DropDown.css';
+import './UserDropDown.css';
 import UserContext from '../../../Context/UserContext';
 
-const DropDown = ({ closeDropDown }) => {
+const UserDropDown = ({ closeUserDropDown }) => {
   const history = useHistory();
   const { user } = useContext(UserContext);
   const dropDown = useRef();
@@ -17,21 +17,21 @@ const DropDown = ({ closeDropDown }) => {
         !dropDown.current.contains(e.target) &&
         e.target.className !== 'profile-picture'
       )
-        closeDropDown();
+        closeUserDropDown();
     };
 
     document.addEventListener('mousedown', handleClickOutside);
 
     return () => document.removeEventListener('mousedown', handleClickOutside);
-  }, [closeDropDown]);
+  }, [closeUserDropDown]);
 
   return (
-    <div className='drop-down-container'>
+    <div className='user-drop-down-container'>
       <div ref={dropDown} className='user-drop-down'>
         <button
           type='button'
           onClick={() => {
-            closeDropDown();
+            closeUserDropDown();
             history.push(`/${user.displayName}`);
           }}
         >
@@ -40,7 +40,7 @@ const DropDown = ({ closeDropDown }) => {
         <button
           type='button'
           onClick={() => {
-            closeDropDown();
+            closeUserDropDown();
             history.push(`/${user.displayName}/liked`);
           }}
         >
@@ -49,7 +49,7 @@ const DropDown = ({ closeDropDown }) => {
         <button
           type='button'
           onClick={() => {
-            closeDropDown();
+            closeUserDropDown();
             history.push('/settings');
           }}
         >
@@ -70,8 +70,8 @@ const DropDown = ({ closeDropDown }) => {
   );
 };
 
-DropDown.propTypes = {
-  closeDropDown: PropTypes.func.isRequired,
+UserDropDown.propTypes = {
+  closeUserDropDown: PropTypes.func.isRequired,
 };
 
-export default DropDown;
+export default UserDropDown;
