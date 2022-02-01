6159ecf669b5c464bd2797eb063c48f4881bb568
Riley Loudon <rileyloudon@icloud.com>
2022-01-31 21:56:25 -0600

    setup deny follow request

--- src/components/FollowRequests/FollowRequests.jsx
+++ src/components/FollowRequests/FollowRequests.jsx
@@ -1,7 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import UserContext from '../../Context/UserContext';
-import { fetchFollowRequests, acceptFollowRequest } from '../../firebase';
+import {
+  fetchFollowRequests,
+  acceptFollowRequest,
+  denyFollowRequest,
+} from '../../firebase';
 import Loading from '../Loading/Loading';
 import './FollowRequests.css';
 
@@ -10,41 +14,56 @@ const FollowRequests = () => {
   const [followRequests, setFollowRequests] = useState();
   const [loading, setLoading] = useState(true);
 
+  const updateRequests = (userToRemove, i) => {
+    const filteredUsers = followRequests.users.filter(
+      (currentUser) => currentUser.username !== userToRemove
+    );
+    const filteredPictures = followRequests.profilePictures.filter(
+      (picture) => picture !== followRequests.profilePictures[i]
+    );
+
+    setFollowRequests({
+      users: filteredUsers,
+      profilePictures: filteredPictures,
+    });
+
+    setUser((prevState) => ({
+      ...prevState,
+      followRequests: prevState.followRequests - 1,
+    }));
+  };
+
   const requests = () => {
     if (followRequests.users && followRequests.users.length === 0) {
-      return <h2 className='no-requests'>No follow requests to show</h2>;
+      return <h3 className='no-requests'>No follow requests to show</h3>;
     }
     return followRequests.users.map((request, i) => {
       return (
         <div className='request' key={request.username}>
           <Link to={`/${request.username}`}>
             <img src={followRequests.profilePictures[i]} alt='' />
-            <p>{request.username}</p>
+            <span>{request.username}</span>
           </Link>
           <button
             className='accept-request'
             type='button'
             onClick={() => {
-              acceptFollowRequest(request.username).then((res) => {
-                setUser(res);
-
-                const filteredUsers = followRequests.users.filter(
-                  (currentUser) => currentUser.username !== request.username
-                );
-                const filteredPictures = followRequests.profilePictures.filter(
-                  (picture) => picture !== followRequests.profilePictures[i]
-                );
-
-                setFollowRequests({
-                  users: filteredUsers,
-                  profilePictures: filteredPictures,
-                });
+              acceptFollowRequest(request.username).then(() => {
+                updateRequests(request.username, i);
               });
             }}
           >
             Accept
           </button>
-          <button className='deny-request' type='button'>
+          <button
+            className='deny-request'
+            type='button'
+            onClick={() => {
+              denyFollowRequest(request.username).then(() => {
+                updateRequests(request.username, i);
+              });
+            }}
+          >
             Deny
           </button>
         </div>
