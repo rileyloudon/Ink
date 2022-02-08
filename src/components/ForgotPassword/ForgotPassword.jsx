4313b9f024d27e28b9cdb125a348793609bb9611
Riley Loudon <rileyloudon@icloud.com>
2022-02-07 20:03:13 -0600

    setup forgot password

---
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Spinner } from '../../img/spinner/spinner.svg';
+import './ForgotPassword.css';
+import { forgotPassword } from '../../firebase';
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [emailSent, setEmailSent] = useState(false);
+  const [resetPasswordError, setResetPasswordError] = useState('');
+  const [buttonLoading, setButtonLoading] = useState(false);
+
+  const isFormValid = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g.test(email);
+
+  const handleReset = async () => {
+    setButtonLoading(true);
+    const status = await forgotPassword(email);
+    if (status === 'Email sent') {
+      setEmailSent(true);
+    } else {
+      setResetPasswordError(status);
+      setButtonLoading(false);
+    }
+  };
+
+  return (
+    <div className='forgot-password-container'>
+      <div className='forgot-password'>
+        <h1 className='ink'>Ink</h1>
+        {!emailSent ? (
+          <form>
+            <input
+              className='username'
+              type='email'
+              aria-label='Email'
+              placeholder='Email'
+              autoCorrect='off'
+              aria-required='true'
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button
+              className='reset-btn'
+              type='submit'
+              disabled={!isFormValid}
+              onClick={handleReset}
+            >
+              {!buttonLoading ? 'Reset Password' : ``}
+              {buttonLoading && <Spinner className='spinner' />}
+            </button>
+            <p className='reset-error' role='alert'>
+              {resetPasswordError}
+            </p>
+          </form>
+        ) : (
+          <div className='email-sent'>
+            <h2>Email sent</h2>
+            <Link to='/'>Go Back</Link>
+          </div>
+        )}
+      </div>
+      <div className='other-action'>
+        <Link to='/'>Sign In</Link>
+        <Link to='/register'> Sign up</Link>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
