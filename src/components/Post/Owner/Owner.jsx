698c05888154f38de0224038bea20ad7eec5f52a
Riley Loudon <rileyloudon@icloud.com>
2022-02-16 22:37:15 -0600

    setup dots svg

--- src/components/Post/Owner/Owner.jsx
+++ src/components/Post/Owner/Owner.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Dots } from '../../../img/dots/dots.svg';
+import PostDropDown from '../PostDropDown/PostDropDown';
 import './Owner.css';
 
 const Owner = ({ owner, profilePicture }) => {
+  const [displayPostDropdown, setDisplayPostDropdown] = useState(false);
+
+  const closePostDropdown = () => setDisplayPostDropdown(false);
+
   return (
     <section className='owner'>
       <Link to={`/${owner}`}>
@@ -12,6 +18,17 @@ const Owner = ({ owner, profilePicture }) => {
       <Link to={`/${owner}`}>
         <span>{owner}</span>
       </Link>
+      <Dots
+        className='dots'
+        onClick={() =>
+          displayPostDropdown
+            ? setDisplayPostDropdown(false)
+            : setDisplayPostDropdown(true)
+        }
+      />
+      {displayPostDropdown && (
+        <PostDropDown closePostDropdown={closePostDropdown} />
+      )}
     </section>
   );
 };
