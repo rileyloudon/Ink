5958f9d8c59af298d975cdeefdb6f0aa27e11daa
Riley Loudon <rileyloudon@icloud.com>
2022-02-24 23:11:52 -0600

    fix dropdown spelling, move ref

--- src/components/Header/UserDropDown/UserDropDown.jsx
+++ src/components/Header/UserDropDown/UserDropDown.jsx
@@ -1,5 +1,4 @@
 import { useContext, useRef } from 'react';
-// import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { signOutUser } from '../../../firebase';
 import UserContext from '../../../Context/UserContext';
@@ -8,12 +7,12 @@ import './UserDropdown.css';
 const UserDropdown = () => {
   const history = useHistory();
   const { user } = useContext(UserContext);
-  const userDropDown = useRef();
+  const userDropdown = useRef();
 
   const handleClickOutside = (e) => {
     if (
-      userDropDown.current &&
-      !userDropDown.current.contains(e.target) &&
+      userDropdown.current &&
+      !userDropdown.current.contains(e.target) &&
       e.target.className !== 'profile-picture' &&
       document.getElementById(`user-dropdown`).classList.contains('displayed')
     ) {
@@ -23,12 +22,12 @@ const UserDropdown = () => {
   };
 
   const handleClick = () => {
-    const post = document.getElementById(`user-dropdown`);
+    const dropdown = document.getElementById(`user-dropdown`);
 
-    if (post.classList.contains('displayed'))
+    if (dropdown.classList.contains('displayed'))
       document.removeEventListener('mousedown', handleClickOutside);
     else document.addEventListener('mousedown', handleClickOutside);
-    post.classList.toggle('displayed');
+    dropdown.classList.toggle('displayed');
   };
 
   const removeEverything = () => {
@@ -37,11 +36,11 @@ const UserDropdown = () => {
   };
 
   return (
-    <div className='user-dropdown-container'>
+    <div ref={userDropdown} className='user-dropdown-container'>
       <button type='button' className='profile-border' onClick={handleClick}>
         <img className='profile-picture' src={user.photoURL} alt='' />
       </button>
-      <div ref={userDropDown} id='user-dropdown'>
+      <div id='user-dropdown'>
         <button
           type='button'
           onClick={() => {
@@ -84,9 +83,5 @@ const UserDropdown = () => {
     </div>
   );
 };
-
-// UserDropdown.propTypes = {
-//   closeUserDropdown: PropTypes.func.isRequired,
-// };
 
 export default UserDropdown;
