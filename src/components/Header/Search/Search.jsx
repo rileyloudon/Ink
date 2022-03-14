15e5133a11c4a3f14f51b6e32fa06b61f8dddc1f
Riley Loudon <rileyloudon@icloud.com>
2022-03-13 19:52:09 -0500

    fix search results not being clickable

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { searchUsers } from '../../../firebase';
 import './Search.css';
 
 const Search = () => {
+  const search = useRef();
   const [searchString, setSearchString] = useState('');
   const [results, setResults] = useState();
 
@@ -14,28 +15,61 @@ const Search = () => {
     });
   };
 
+  const handleClickOutside = (e) => {
+    if (
+      search.current &&
+      !search.current.contains(e.target) &&
+      document.getElementById(`search-results`).classList.contains('displayed')
+    ) {
+      document.getElementById(`search-results`).classList.remove('displayed');
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  };
+
+  const handleClick = () => {
+    const searchResults = document.getElementById(`search-results`);
+
+    if (searchResults.classList.contains('displayed'))
+      document.removeEventListener('mousedown', handleClickOutside);
+    else
+      document.addEventListener('mousedown', handleClickOutside, {
+        once: true,
+      });
+    searchResults.classList.toggle('displayed');
+  };
+
+  const removeEverything = () => {
+    document.removeEventListener('mousedown', handleClickOutside);
+    document.getElementById(`search-results`).classList.remove('displayed');
+  };
+
   const displaySearchResult = (user) => {
     return (
-      <div className='search-user' key={user.username}>
-        <Link to={`/${user.username}`}>
-          <img src={user.photoURL} alt='' />
-          <span>{user.username}</span>
-        </Link>
-      </div>
+      <Link
+        to={`/${user.username}`}
+        key={user.username}
+        onClick={removeEverything}
+      >
+        <img src={user.photoURL} alt='' />
+        <span>{user.username}</span>
+      </Link>
     );
   };
 
   return (
-    <div className='search'>
+    <div ref={search} className='search'>
       <input
         className='search-box'
         type='text'
         placeholder='Search'
         autoCapitalize='none'
         value={searchString}
+        onClick={handleClick}
         onChange={(e) => handleSearch(e)}
       />
-      {results && results.map((user) => displaySearchResult(user))}
+      <div id='search-results'>
+        {results && results.map((user) => displaySearchResult(user))}
+      </div>
     </div>
   );
 };
