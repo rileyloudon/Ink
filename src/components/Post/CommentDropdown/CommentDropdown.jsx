0c03392543f27309072a1e52f98215c25e703637
Riley Loudon <rileyloudon@icloud.com>
2022-04-28 21:48:26 -0500

    setup deleteSelectedComment

--- src/components/Post/CommentDropdown/CommentDropdown.jsx
+++ src/components/Post/CommentDropdown/CommentDropdown.jsx
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types';
 import { useRef, useState, useEffect } from 'react';
+import { deleteComment } from '../../../firebase';
 import { ReactComponent as Dots } from '../../../img/dots/dots.svg';
 import './CommentDropdown.css';
 
-const CommentDropdown = ({ comment, style }) => {
+const CommentDropdown = ({ post, comment, style, deleteDisplayedComment }) => {
   const commentDropdownRef = useRef(null);
   const [commentDropdownOpen, setCommentDropdownOpen] = useState(false);
 
   const handleClick = () => setCommentDropdownOpen(!commentDropdownOpen);
+
+  const deleteSelectedComment = async () => {
+    await deleteComment(post, comment);
+    deleteDisplayedComment(comment);
+  };
 
   useEffect(() => {
     const handleClickOutside = (e) => {
@@ -24,6 +30,10 @@ const CommentDropdown = ({ comment, style }) => {
     return () => window.removeEventListener('click', handleClickOutside);
   }, [commentDropdownOpen]);
 
+  useEffect(() => {
+    if (style.display === 'none') setCommentDropdownOpen(false);
+  }, [style]);
+
   return (
     <div className='comment-dropdown-container' style={style}>
       <Dots className='dots' onClick={handleClick} />
@@ -31,13 +41,7 @@ const CommentDropdown = ({ comment, style }) => {
         ref={commentDropdownRef}
         className={`comment-dropdown ${commentDropdownOpen ? 'displayed' : ''}`}
       >
-        <button
-          type='button'
-          onClick={() => {
-            // Delete Comment
-            console.log(comment);
-          }}
-        >
+        <button type='button' onClick={() => deleteSelectedComment()}>
           Delete Comment
         </button>
       </div>
@@ -46,8 +50,12 @@ const CommentDropdown = ({ comment, style }) => {
 };
 
 CommentDropdown.propTypes = {
+  post: PropTypes.shape({}).isRequired,
   comment: PropTypes.shape({}).isRequired,
-  style: PropTypes.shape({}).isRequired,
+  style: PropTypes.shape({
+    display: PropTypes.string.isRequired,
+  }).isRequired,
+  deleteDisplayedComment: PropTypes.func.isRequired,
 };
 
 export default CommentDropdown;
