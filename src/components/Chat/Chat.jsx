8e1f0da9607872f9a48026ffd0f074d324f812e2
Riley Loudon <rileyloudon@icloud.com>
2022-05-06 19:27:04 -0500

    start setting up Chat

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+import UserSelector from './UserSelector/UserSelector';
+import Messages from './Messages/Messages';
 import './Chat.css';
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const [currentSelectedUser, setCurrentSelectedUser] = useState(null);
+
+  const updateCurrentSelectedUser = (value) => setCurrentSelectedUser(value);
+
+  return (
+    <div className='chat'>
+      <UserSelector
+        currentSelectedUser={currentSelectedUser}
+        updateCurrentSelectedUser={updateCurrentSelectedUser}
+      />
+      <Messages currentSelectedUser={currentSelectedUser} />
+    </div>
+  );
 };
 
 export default Chat;
