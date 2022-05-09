17e06aba81d8c854c1ef81cbf18d8d8ace0d2197
Riley Loudon <rileyloudon@icloud.com>
2022-05-08 20:08:11 -0500

    update Search path, fix name of set state function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,20 +3,20 @@ import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import FavoriteDropdown from './FavoriteDropdown/FavoriteDropdown';
 import UserDropdown from './UserDropdown/UserDropdown';
+import Search from '../Search/Search';
 import { ReactComponent as HomeOutline } from '../../img/home/home-outline.svg';
 import { ReactComponent as HomeFilled } from '../../img/home/home-filled.svg';
 import { ReactComponent as ChatOutline } from '../../img/chat/chat-outline.svg';
 import { ReactComponent as ChatFilled } from '../../img/chat/chat-filled.svg';
 import { ReactComponent as AddOutline } from '../../img/add/add-outline.svg';
 import { ReactComponent as AddFilled } from '../../img/add/add-filled.svg';
-import Search from './Search/Search';
 import './header.css';
 
 const Header = ({ updateAddModal, showAddModal }) => {
   const location = useLocation();
   const [favoriteDropdownOpen, setFavoriteDropdownOpen] = useState(false);
 
-  const changeFavoriteDropdownOpen = (value) => setFavoriteDropdownOpen(value);
+  const updateFavoriteDropdownOpen = (value) => setFavoriteDropdownOpen(value);
 
   return (
     <header>
@@ -59,7 +59,7 @@ const Header = ({ updateAddModal, showAddModal }) => {
 
             <FavoriteDropdown
               favoriteDropdownOpen={favoriteDropdownOpen}
-              changeFavoriteDropdownOpen={changeFavoriteDropdownOpen}
+              updateFavoriteDropdownOpen={updateFavoriteDropdownOpen}
             />
             <UserDropdown />
           </div>
