5ecc5fc4134d910acc1a8a07e242db80c4f65b2f
Riley Loudon <rileyloudon@icloud.com>
2022-05-09 19:09:36 -0500

    start setting up Messages

--- src/components/Chat/Messages/Messages.jsx
+++ src/components/Chat/Messages/Messages.jsx
@@ -1,7 +1,42 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AddComment from '../../Post/AddComment/AddComment';
 import './Messages.css';
 
-const Messages = () => {
-  return <div>Messages</div>;
+const Messages = ({ currentSelectedUser }) => {
+  const [prevMessages] = useState(null);
+
+  return (
+    <div className='messages'>
+      {currentSelectedUser ? (
+        <Link
+          to={`/${currentSelectedUser.username}`}
+          key={currentSelectedUser.username}
+        >
+          <img src={currentSelectedUser.photoURL} alt='' />
+          <span>{currentSelectedUser.username}</span>
+        </Link>
+      ) : (
+        <p>No User Selected</p>
+      )}
+      <div className='prev-messages'>
+        {prevMessages && prevMessages.map((message) => <p>{message}</p>)}
+      </div>
+      <AddComment chat />
+    </div>
+  );
 };
 
 export default Messages;
+
+Messages.defaultProps = {
+  currentSelectedUser: null,
+};
+
+Messages.propTypes = {
+  currentSelectedUser: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    photoURL: PropTypes.string.isRequired,
+  }),
+};
