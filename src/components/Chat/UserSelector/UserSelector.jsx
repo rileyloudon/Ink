5531abe3527af2052e907abdc7d705f5696d786a
Riley Loudon <rileyloudon@icloud.com>
2022-05-17 20:10:50 -0500

    display past chats

--- src/components/Chat/UserSelector/UserSelector.jsx
+++ src/components/Chat/UserSelector/UserSelector.jsx
@@ -1,7 +1,88 @@
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import { fetchLatestChatUsers } from '../../../firebase';
+import Search from '../../Search/Search';
 import './UserSelector.css';
 
-const UserSelector = () => {
-  return <div>UserSelector</div>;
+const UserSelector = ({ currentSelectedUser, updateCurrentSelectedUser }) => {
+  const [pastChats, setPastChats] = useState(null);
+
+  useEffect(() => {
+    if (currentSelectedUser) {
+      if (!pastChats) {
+        setPastChats({
+          users: currentSelectedUser.username,
+          profilePictures: currentSelectedUser.photoURL,
+        });
+      } else if (!pastChats.users.includes(currentSelectedUser.username)) {
+        setPastChats((prevState) => ({
+          users: [currentSelectedUser.username, ...prevState.users],
+          profilePictures: [
+            currentSelectedUser.photoURL,
+            ...prevState.profilePictures,
+          ],
+        }));
+      }
+    }
+  }, [currentSelectedUser, pastChats]);
+
+  useEffect(() => {
+    let isSubscribed = true;
+    console.log('s');
+
+    (async () => {
+      // res returns an object: users & profilePictures
+      // thry will be in the same order so users[0] goes with profilePictures[0]
+      const res = await fetchLatestChatUsers();
+      if (isSubscribed) {
+        setPastChats(res);
+      }
+    })();
+
+    return () => {
+      isSubscribed = false;
+    };
+  }, []);
+
+  return pastChats ? (
+    <div className='user-selector'>
+      <h3>Chat</h3>
+      <h4>To</h4>
+      <Search updateCurrentSelectedUser={updateCurrentSelectedUser} />
+      {/* Search box to find users to chat to. Check 'allowMessages' user value */}
+      <h4>Past Chats</h4>
+      {pastChats.users.map((user, i) => {
+        return (
+          <button
+            className='past-chat'
+            type='button'
+            key={user}
+            onClick={() =>
+              updateCurrentSelectedUser({
+                username: user,
+                photoURL: pastChats.profilePictures[i],
+              })
+            }
+          >
+            <img src={pastChats.profilePictures[i]} alt='' />
+            <span>{user}</span>
+          </button>
+        );
+      })}
+    </div>
+  ) : null;
 };
 
 export default UserSelector;
+
+UserSelector.defaultProps = {
+  currentSelectedUser: null,
+};
+
+UserSelector.propTypes = {
+  currentSelectedUser: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    photoURL: PropTypes.string.isRequired,
+  }),
+  updateCurrentSelectedUser: PropTypes.func.isRequired,
+};
