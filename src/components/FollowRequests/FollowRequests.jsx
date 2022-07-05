7064e9de2f1ea98bc9ea2a24bc3ad6b70242b0d1
Riley Loudon <rileyloudon@icloud.com>
2022-07-04 19:42:23 -0500

    add loading spinner to buttons

--- src/components/FollowRequests/FollowRequests.jsx
+++ src/components/FollowRequests/FollowRequests.jsx
@@ -7,12 +7,17 @@ import {
   denyFollowRequest,
 } from '../../firebase';
 import Loading from '../Loading/Loading';
+import { ReactComponent as Spinner } from '../../img/spinner/spinner.svg';
 import './FollowRequests.css';
 
 const FollowRequests = () => {
   const { user, setUser } = useContext(UserContext);
   const [followRequests, setFollowRequests] = useState();
   const [loading, setLoading] = useState(true);
+  const [buttonLoading, setButtongLoading] = useState({
+    accept: false,
+    deny: false,
+  });
 
   const updateRequests = (userToRemove, i) => {
     const filteredUsers = followRequests.users.filter(
@@ -51,21 +56,27 @@ const FollowRequests = () => {
               className='accept-request'
               type='button'
               onClick={async () => {
+                setButtongLoading({ accept: true, deny: false });
                 await acceptFollowRequest(request.username);
                 updateRequests(request.username, i);
               }}
             >
-              Accept
+              {buttonLoading.accept ? (
+                <Spinner className='spinner' />
+              ) : (
+                'Accept'
+              )}
             </button>
             <button
               className='deny-request'
               type='button'
               onClick={async () => {
+                setButtongLoading({ accept: false, deny: true });
                 await denyFollowRequest(request.username);
                 updateRequests(request.username, i);
               }}
             >
-              Deny
+              {buttonLoading.deny ? <Spinner className='spinner' /> : 'Deny'}
             </button>
           </div>
         ))}
@@ -77,6 +88,8 @@ const FollowRequests = () => {
     let isSubscribed = true;
     if (user)
       (async () => {
+        // res returns an object: users & profilePictures
+        // thry will be in the same order so users[0] goes with profilePictures[0]
         const res = await fetchFollowRequests();
         if (isSubscribed) {
           setFollowRequests(res);
