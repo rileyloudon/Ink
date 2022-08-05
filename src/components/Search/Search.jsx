96bfeffed811a4225e09b6f4df61e80cd93386a3
Riley Loudon <rileyloudon@icloud.com>
2022-08-04 22:59:24 -0500

    fix results not closing on mobile safari

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { searchUsers } from '../../firebase';
@@ -10,15 +10,25 @@ const Search = ({ updateCurrentSelectedUser, updateActiveTab }) => {
   const [results, setResults] = useState();
   const [resultsOpen, setResultsOpen] = useState(false);
 
-  const handleFocus = (e) => e.target.select();
-
-  const handleClick = async () => {
+  const handleFocus = async (e) => {
     setResultsOpen(true);
+
+    // setTimeout required to select existing text in Safari
+    setTimeout(() => {
+      if (searchString.length > 0) e.target.select();
+    }, 10);
 
     if (!results) {
       const matchingUsers = await searchUsers('');
       setResults(matchingUsers);
     }
+  };
+
+  const handleBlur = () => {
+    // allows for click events then closes results
+    setTimeout(() => {
+      setResultsOpen(false);
+    }, 10);
   };
 
   const handleSearch = async (e) => {
@@ -27,21 +37,6 @@ const Search = ({ updateCurrentSelectedUser, updateActiveTab }) => {
     const matchingUsers = await searchUsers(e.target.value.toLowerCase());
     setResults(matchingUsers);
   };
-
-  useEffect(() => {
-    const handleClickOutside = (e) => {
-      if (
-        searchResultsRef.current !== null &&
-        !searchResultsRef.current.contains(e.traget) &&
-        e.target.className !== 'search-box'
-      ) {
-        setResultsOpen(false);
-      }
-    };
-
-    if (resultsOpen) window.addEventListener('click', handleClickOutside);
-    return () => window.removeEventListener('click', handleClickOutside);
-  }, [resultsOpen]);
 
   const displaySearchResult = (user) => {
     if (updateCurrentSelectedUser) {
@@ -84,9 +79,9 @@ const Search = ({ updateCurrentSelectedUser, updateActiveTab }) => {
         placeholder='Search'
         autoCapitalize='none'
         value={searchString}
-        onClick={handleClick}
         onChange={(e) => handleSearch(e)}
-        onFocus={handleFocus}
+        onFocus={(e) => handleFocus(e)}
+        onBlur={handleBlur}
       />
       <div
         ref={searchResultsRef}
