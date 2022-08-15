71da133eb1671543f89dd2194633817213cb6783
Riley Loudon <rileyloudon@icloud.com>
2022-08-14 21:36:26 -0500

    update for React 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
