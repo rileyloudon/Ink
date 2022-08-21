cabf5f16fe55b99696ebab4925554d187d8e3fe9
Riley Loudon <rileyloudon@icloud.com>
2022-08-20 22:12:01 -0500

    prevent firebase auto sign in

--- cypress/e2e/SignIn.cy.js
+++ cypress/e2e/SignIn.cy.js
@@ -2,13 +2,14 @@ import SignIn from '../fixtures/SignIn.json';
 
 describe('Both user and guest sign ins work', () => {
   before(() => {
+    indexedDB.deleteDatabase('firebaseLocalStorageDb');
     cy.visit('/');
   });
 
   it('Guest sign in works', () => {
     cy.get('.guest-btn').click();
     cy.get('.profile-picture').click();
-    cy.get('[href="#/guest"]').click();
+    cy.get('.user-dropdown > [href="#/guest"]').click();
     cy.get('.username').should('contain.text', 'guest');
   });
 
@@ -19,13 +20,14 @@ describe('Both user and guest sign ins work', () => {
   });
 
   it('User sign in works', () => {
+    const { email, password, username } = SignIn;
     // This test will fail without an account saved in fixtures
     // in 'cypress' folder, create 'fixtures' folder -> create SignIn.json -> add email, password, and username of an account to test
-    cy.get('.username').type(SignIn.email);
-    cy.get('[type="password"]').type(SignIn.password);
+    cy.get('.username').type(email);
+    cy.get('[type="password"]').type(password);
     cy.get('.sign-in-btn').click();
     cy.get('.profile-picture').click();
-    cy.get(`[href="#/${SignIn.username}"]`).click();
-    cy.get('.username').should('contain.text', SignIn.username);
+    cy.get(`.user-dropdown > [href="#/${username}"]`).click();
+    cy.get('.username').should('contain.text', username);
   });
 });
