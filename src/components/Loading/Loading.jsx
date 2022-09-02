f14c39ff225ed8f2870647c0d0f6bb3be30d8370
Riley Loudon <rileyloudon@icloud.com>
2022-09-01 19:39:52 -0500

    fix centering on small screens

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,11 +2,7 @@ import logo from '../../img/misc/octopus-small.png';
 import './Loading.css';
 
 const Loading = () => {
-  return (
-    <div className='loading-logo'>
-      <img src={logo} alt='Loading...' />
-    </div>
-  );
+  return <img src={logo} alt='Loading...' className='loading-logo' />;
 };
 
 export default Loading;
