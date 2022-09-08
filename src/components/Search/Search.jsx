cf3f64e72669af5560c2dbefc2dee9b6b126ae43
Riley Loudon <rileyloudon@icloud.com>
2022-09-07 22:22:33 -0500

    fix user links not working

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { searchUsers } from '../../firebase';
@@ -10,25 +10,13 @@ const Search = ({ updateCurrentSelectedUser, updateActiveTab }) => {
   const [results, setResults] = useState();
   const [resultsOpen, setResultsOpen] = useState(false);
 
-  const handleFocus = async (e) => {
+  const handleClick = async () => {
     setResultsOpen(true);
-
-    // setTimeout required to select existing text in Safari
-    setTimeout(() => {
-      if (searchString.length > 0) e.target.select();
-    }, 10);
 
     if (!results) {
       const matchingUsers = await searchUsers('');
       setResults(matchingUsers);
     }
-  };
-
-  const handleBlur = () => {
-    // allows for click events then closes results
-    setTimeout(() => {
-      setResultsOpen(false);
-    }, 10);
   };
 
   const handleSearch = async (e) => {
@@ -37,6 +25,24 @@ const Search = ({ updateCurrentSelectedUser, updateActiveTab }) => {
     const matchingUsers = await searchUsers(e.target.value.toLowerCase());
     setResults(matchingUsers);
   };
+
+  // focus any text currently in search box
+  const handleFocus = (e) => e.target.select();
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (
+        searchResultsRef.current !== null &&
+        !searchResultsRef.current.contains(e.traget) &&
+        e.target.className !== 'search-box'
+      ) {
+        setResultsOpen(false);
+        document.querySelector('.search-box').blur();
+      }
+    };
+    if (resultsOpen) window.addEventListener('click', handleClickOutside);
+    return () => window.removeEventListener('click', handleClickOutside);
+  }, [resultsOpen]);
 
   const displaySearchResult = (user) => {
     if (updateCurrentSelectedUser) {
@@ -79,9 +85,9 @@ const Search = ({ updateCurrentSelectedUser, updateActiveTab }) => {
         placeholder='Search'
         autoCapitalize='none'
         value={searchString}
+        onClick={handleClick}
         onChange={(e) => handleSearch(e)}
-        onFocus={(e) => handleFocus(e)}
-        onBlur={handleBlur}
+        onFocus={handleFocus}
       />
       <div
         ref={searchResultsRef}
