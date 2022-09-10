890900f2fd74b9b41a415c3a3b8e57256ff288c0
Riley Loudon <rileyloudon@icloud.com>
2022-09-09 23:24:08 -0500

    add backdrop when loading modal posts

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,8 +1,22 @@
+import { PropTypes } from 'prop-types';
 import logo from '../../img/misc/octopus-small.png';
 import './Loading.css';
 
-const Loading = () => {
-  return <img src={logo} alt='Loading...' className='loading-logo' />;
+const Loading = ({ modal }) => {
+  return (
+    <>
+      {modal && <div className='loading-backdrop' />}
+      <img src={logo} alt='Loading...' className='loading-logo' />
+    </>
+  );
+};
+
+Loading.defaultProps = {
+  modal: false,
+};
+
+Loading.propTypes = {
+  modal: PropTypes.bool,
 };
 
 export default Loading;
