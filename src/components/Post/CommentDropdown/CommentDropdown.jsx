13bc148aa3908aa47fa63c4b6592bcdf59f99edc
Riley Loudon <rileyloudon@icloud.com>
2022-09-11 23:05:18 -0500

    fix dropdown not displaying

--- src/components/Post/CommentDropdown/CommentDropdown.jsx
+++ src/components/Post/CommentDropdown/CommentDropdown.jsx
@@ -6,20 +6,21 @@ import './CommentDropdown.css';
 
 const CommentDropdown = ({ post, comment, style, deleteDisplayedComment }) => {
   const commentDropdownRef = useRef(null);
-  const [commentDropdownOpen, setCommentDropdownOpen] = useState(false);
+  const [commentDropdownOpen, setCommentDropdownOpen] = useState(true);
 
   const handleClick = () => setCommentDropdownOpen(!commentDropdownOpen);
 
   const deleteSelectedComment = async () => {
-    await deleteComment(post, comment);
     deleteDisplayedComment(comment);
+    await deleteComment(post, comment);
   };
 
   useEffect(() => {
     const handleClickOutside = (e) => {
       if (
         commentDropdownRef.current !== null &&
-        !commentDropdownRef.current.contains(e.traget)
+        !commentDropdownRef.current.contains(e.traget) &&
+        !e.target.classList.contains('comment-dots')
       )
         setCommentDropdownOpen(!commentDropdownOpen);
     };
@@ -35,15 +36,16 @@ const CommentDropdown = ({ post, comment, style, deleteDisplayedComment }) => {
 
   return (
     <div className='comment-dropdown-container' style={style}>
-      <Dots className='dots' onClick={handleClick} />
-      <div
-        ref={commentDropdownRef}
-        className={`comment-dropdown ${commentDropdownOpen ? 'displayed' : ''}`}
-      >
-        <button type='button' onClick={() => deleteSelectedComment()}>
-          Delete Comment
-        </button>
-      </div>
+      <button type='button' className='comment-dots' onClick={handleClick}>
+        <Dots className='dots' />
+      </button>
+      {commentDropdownOpen && (
+        <div ref={commentDropdownRef} className='comment-dropdown'>
+          <button type='button' onClick={() => deleteSelectedComment()}>
+            Delete Comment
+          </button>
+        </div>
+      )}
     </div>
   );
 };
