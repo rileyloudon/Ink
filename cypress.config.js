bd1aeb55a8f99e7a3ee9fcade88b509154e1029e
Riley Loudon <rileyloudon@icloud.com>
2022-09-16 21:23:48 -0500

    add missing '#' to baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000/ink/',
+    baseUrl: 'http://localhost:3000/ink#/',
   },
 });
